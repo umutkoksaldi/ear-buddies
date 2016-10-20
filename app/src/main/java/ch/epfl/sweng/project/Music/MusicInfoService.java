208bde7aa87557ff4e18af2f7e6ae1717f98031a
merinorus <merinorus.antoine@gmail.com>
2016-10-20 21:25:35 +0200

    Beginning of music service implementation (doesn't work)

---
+++ app/src/main/java/ch/epfl/sweng/project/Music/MusicInfoService.java
@@ -0,0 +1,14 @@
+package ch.epfl.sweng.project.Music;
+
+import android.app.Service;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.os.IBinder;
+import android.util.Log;
+
+public class MusicInfoService extends BroadcastReceiver {
+    String status = null;
+
+
+}
