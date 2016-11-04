36066c35ca772320e3c1c050255edce8463ae01a
Etienne Hüsler <etiuse@windowslive.com>
2016-11-04 14:27:24 +0100

    Corrected tests

---
+++ app/src/androidTest/java/ch/epfl/sweng/project/ModuleRequest/TestUI.java
@@ -0,0 +1,99 @@
+package ch.epfl.sweng.project.ModuleRequest;
+
+import android.os.SystemClock;
+import android.support.test.InstrumentationRegistry;
+import android.test.ActivityInstrumentationTestCase2;
+import android.view.View;
+
+import org.hamcrest.Matcher;
+
+import ch.epfl.sweng.project.MainActivity;
+import ch.epfl.sweng.project.Model.Location;
+import ch.epfl.sweng.project.Model.ModelApplication;
+import ch.epfl.sweng.project.Model.User;
+import ch.epfl.sweng.project.R;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.action.ViewActions.swipeLeft;
+import static android.support.test.espresso.action.ViewActions.swipeRight;
+
+import static android.support.test.espresso.matcher.ViewMatchers.isDescendantOfA;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import static android.support.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.Matchers.allOf;
+
+/**
+ * Created by Etienne on 28.10.2016.
+ */
+
+public class TestUI extends ActivityInstrumentationTestCase2<MainActivity> {
+
+    public TestUI() {
+        super(MainActivity.class);
+    }
+    private final ModelApplication modelApplication = ModelApplication.getModelApplication();
+
+
+    @Override
+    public void setUp(){
+        try {
+            super.setUp();
+        } catch (java.lang.Exception e){}
+        User userTest = new User();
+        userTest.setLocation(new Location());
+        modelApplication.setUser(userTest);
+        getActivity();
+        injectInstrumentation(InstrumentationRegistry.getInstrumentation());
+    }
+
+    public void testCanClickFromMapToBlank() {
+        SystemClock.sleep(2000);
+        Matcher<View> matcher = allOf(withText("Users"),
+                isDescendantOfA(withId(R.id.tabLayoutMain)));
+        SystemClock.sleep(500);
+        onView(matcher).perform(click());
+    }
+
+    public void testCanClickFromMapToProfile() {
+        getActivity();
+        SystemClock.sleep(2000);
+        Matcher<View> matcher = allOf(withText("Profile"),
+                isDescendantOfA(withId(R.id.tabLayoutMain)));
+        SystemClock.sleep(500);
+        onView(matcher).perform(click());
+    }
+
+    public void testCanClickOnAllFragments() {
+        SystemClock.sleep(2000);
+        Matcher<View> matcher = allOf(withText("Users"),
+                isDescendantOfA(withId(R.id.tabLayoutMain)));
+        SystemClock.sleep(500);
+        onView(matcher).perform(click());
+        matcher = allOf(withText("Map"),
+                isDescendantOfA(withId(R.id.tabLayoutMain)));
+        SystemClock.sleep(500);
+        onView(matcher).perform(click());
+        matcher = allOf(withText("Profile"),
+                isDescendantOfA(withId(R.id.tabLayoutMain)));
+        SystemClock.sleep(500);
+        onView(matcher).perform(click());
+        matcher = allOf(withText("Map"),
+                isDescendantOfA(withId(R.id.tabLayoutMain)));
+        SystemClock.sleep(500);
+        onView(matcher).perform(click());
+    }
+
+    public void testCanSwipeOnMap() {
+        //Visual testing
+        getActivity();
+        SystemClock.sleep(2000);
+        onView(withId(R.id.pagerMain)).perform(swipeRight());
+        SystemClock.sleep(500);
+        onView(withId(R.id.pagerMain)).perform(swipeLeft());
+        onView(withId(R.id.pagerMain)).perform(swipeLeft());
+        SystemClock.sleep(500);
+        onView(withId(R.id.pagerMain)).perform(swipeRight());
+        SystemClock.sleep(5000);
+    }
+}
