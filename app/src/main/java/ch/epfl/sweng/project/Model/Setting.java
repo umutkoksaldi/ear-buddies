bfcab01fc17a64e1ba7c739276df95ff657452f5
Arnaud <arnaud.dupeyrat@gmail.com>
2016-11-08 15:41:40 +0100

    add session tests

---
+++ app/src/main/java/ch/epfl/sweng/project/Model/Setting.java
@@ -0,0 +1,55 @@
+package ch.epfl.sweng.project.Model;
+
+/**
+ * Created by arnauddupeyrat on 08/11/16.
+ */
+
+public class Setting {
+
+    private int ageMin;
+    private int ageMax;
+    private int radius;
+
+    public Setting(int ageMin, int ageMax, int radius) {
+        this.ageMin = ageMin;
+        this.ageMax = ageMax;
+        this.radius = radius;
+    }
+    public Setting(){}
+
+    public int getAgeMax() {
+        return ageMax;
+    }
+
+    public void setAgeMax(int ageMax) {
+        this.ageMax = ageMax;
+    }
+
+    public int getAgeMin() {
+        return ageMin;
+    }
+
+    public void setAgeMin(int ageMin) {
+        this.ageMin = ageMin;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+
+
+
+    // The method should be overridden for jackson.
+    @Override
+    public String toString() {
+        return "{" +
+                " \"ageMin\" : " + ageMin +
+                ", \"ageMax\": " + ageMax +
+                ", \"radius\": " + radius +
+                '}';
+    }
+}
