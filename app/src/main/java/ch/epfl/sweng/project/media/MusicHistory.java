c1feb56883b6f902f6ce6c841668841611da6767
merinorus <merinorus.antoine@gmail.com>
2016-11-25 19:49:18 +0100

    Error : cannot read json file for the music history GET request. Assumption : the server returns a table (starting with the "[" character) instead of an object (starting with a "(" character).

--- app/src/main/java/ch/epfl/sweng/project/media/MusicHistory.java
+++ app/src/main/java/ch/epfl/sweng/project/media/MusicHistory.java
@@ -1,9 +1,16 @@
 package ch.epfl.sweng.project.media;
 
+import android.util.Log;
+
+import org.springframework.http.ResponseEntity;
+
 import java.util.ArrayList;
 
 import Util.GlobalSetting;
+import ch.epfl.sweng.project.Model.ModelApplication;
 import ch.epfl.sweng.project.Model.Music;
+import ch.epfl.sweng.project.ServerRequest.OnServerRequestComplete;
+import ch.epfl.sweng.project.ServerRequest.ServiceHandler;
 
 /**
  * Created by Antoine Merino on 18/11/2016.
@@ -14,6 +21,7 @@ public final class MusicHistory {
     private static MusicHistory musicHistory = null;
     private int length = GlobalSetting.MUSIC_HISTORY_MAX_LENGTH;
     private ArrayList<Music> musicHistoryList = new ArrayList<>();
+    private ModelApplication modelApplication = ModelApplication.getModelApplication();
 
     private MusicHistory() {
     }
@@ -52,10 +60,42 @@ public final class MusicHistory {
         musicHistoryList.add(new Music("Rihanna", "Love the way you lie"));
         musicHistoryList.add(new Music("Rihanna", "Take a bow"));
         musicHistoryList.add(new Music("Rihanna", "Disturbia"));
+
+        sendGet(GlobalSetting.MUSIC_HISTORY_API);
     }
 
     public ArrayList<Music> getHistory() {
         return musicHistoryList;
+    }
+
+    private void sendGet(@SuppressWarnings("SameParameterValue") String
+                                 requestApi) {
+        Log.d("MusicHistory", "sendGet");
+        ServiceHandler serviceHandler = new ServiceHandler(new OnServerRequestComplete() {
+
+            @Override
+            public void onSucess(ResponseEntity response) {
+
+                // We associated the user to the new.
+                if (Integer.parseInt(response.getStatusCode().toString()) == GlobalSetting.GOOD_ANSWER) {
+                    Log.d("MusicHistory", response.getBody().toString());
+                } else {
+                    // Erreur pas pu communiquer avec le serveur
+                    Log.e("MusicHistory", "onSuccess() != Code 200 (good answer)");
+                }
+            }
+
+            @Override
+            public void onFailed() {
+                Log.e("MusicHistory", "onFailed() : could not retreive the info from the server about music history");
+            }
+        });
+
+
+        // the interface is already initiate above
+        String requestURL = GlobalSetting.URL + requestApi + modelApplication.getUser().getIdApiConnection();
+        Log.d("MusicInfoService", "GET Request : " + requestURL);
+        serviceHandler.doGet(requestURL, Music.class);
     }
 
 }
