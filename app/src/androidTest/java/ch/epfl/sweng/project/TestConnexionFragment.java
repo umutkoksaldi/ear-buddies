111ff7c17ca52520cb2391b22dc12811641d0841
Arnaud Dupeyrat <arnaud.dupeyrat@gmail.com>
2016-12-04 12:53:02 +0100

    add description application on login page (#127)

---
+++ app/src/androidTest/java/ch/epfl/sweng/project/TestConnexionFragment.java
@@ -0,0 +1,81 @@
+package ch.epfl.sweng.project;
+
+import android.support.test.InstrumentationRegistry;
+import android.test.ActivityInstrumentationTestCase2;
+
+
+import ch.epfl.sweng.project.Fragment.PresentationAppFragment;
+
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
+import android.app.FragmentManager;
+import android.util.Log;
+
+public class TestConnexionFragment extends ActivityInstrumentationTestCase2<Login> {
+
+    private FragmentManager fragmentManager;
+
+    public TestConnexionFragment() {
+        super(Login.class);
+    }
+
+    @Override
+    public void setUp(){
+        getActivity();
+        injectInsrumentation(InstrumentationRegistry.getInstrumentation());
+        fragmentManager = getActivity().getFragmentManager();
+    }
+
+    public void testPerformClick() {
+
+        try {
+            Thread.sleep(1000);
+            PresentationAppFragment presentationFragmentById = (PresentationAppFragment) fragmentManager.findFragmentById
+                    (R.id.containFragmentPresentation);
+            assertEquals("drawableId should be equals", presentationFragmentById.getDrawableId(), R.mipmap.music_image);
+            assertEquals("drawableId should be equals", presentationFragmentById.getPresentationTextId(),
+                    R.string.presentation_music);
+            assertEquals("drawableId should be equals", presentationFragmentById.getStateLocation(), 1);
+
+            onView(withId(R.id.containFragmentPresentation)).perform(click());
+
+            Thread.sleep(1000);
+            presentationFragmentById = (PresentationAppFragment) fragmentManager.findFragmentById
+                    (R.id.containFragmentPresentation);
+            assertEquals("drawableId should be equals", presentationFragmentById.getDrawableId(), R.mipmap.people_image);
+            assertEquals("drawableId should be equals", presentationFragmentById.getPresentationTextId(),
+                    R.string.presentation_people);
+            assertEquals("drawableId should be equals", presentationFragmentById.getStateLocation(), 2);
+        }
+        catch (Exception e){
+            Log.e("testPerformClick()",e.getMessage());
+            assertFalse(true);
+        }
+    }
+
+
+    public void testperformClicks() {
+        try {
+            onView(withId(R.id.containFragmentPresentation)).perform(click());
+            onView(withId(R.id.containFragmentPresentation)).perform(click());
+            onView(withId(R.id.containFragmentPresentation)).perform(click());
+            onView(withId(R.id.containFragmentPresentation)).perform(click());
+            onView(withId(R.id.containFragmentPresentation)).perform(click());
+
+            Thread.sleep(1000);
+            PresentationAppFragment presentationFragmentById = (PresentationAppFragment) fragmentManager.findFragmentById
+                    (R.id.containFragmentPresentation);
+            assertEquals("drawableId should be equals",presentationFragmentById.getDrawableId(), R.mipmap.interaction_image);
+            assertEquals("drawableId should be equals", presentationFragmentById.getPresentationTextId(),
+                    R.string.presentation_interaction);
+            assertEquals("drawableId should be equals", presentationFragmentById.getStateLocation(),3);
+        }
+        catch (Exception e){
+            Log.e("testPerformClick()",e.getMessage());
+            assertFalse(true);
+        }
+    }
+
+
+}
--- app/src/main/java/ch/epfl/sweng/project/Login.java
+++ app/src/main/java/ch/epfl/sweng/project/Login.java
@@ -1,13 +1,16 @@
 package ch.epfl.sweng.project;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.content.Intent;
-import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.view.View;
+import android.widget.FrameLayout;
 import android.widget.Toast;
-
 import com.facebook.AccessToken;
 import com.facebook.CallbackManager;
 import com.facebook.FacebookCallback;
@@ -18,20 +21,19 @@ import com.facebook.ProfileTracker;
 import com.facebook.login.LoginManager;
 import com.facebook.login.LoginResult;
 import com.facebook.login.widget.LoginButton;
-import com.google.android.gms.appindexing.Action;
 import com.google.android.gms.appindexing.AppIndex;
-import com.google.android.gms.appindexing.Thing;
 import com.google.android.gms.common.api.GoogleApiClient;
-
 import java.lang.ref.WeakReference;
 import java.util.ArrayList;
 import java.util.List;
-
 import Util.GlobalSetting;
 import ch.epfl.sweng.project.Controler.ConnectionControler;
+import ch.epfl.sweng.project.Fragment.PresentationAppFragment;
 import ch.epfl.sweng.project.Model.ModelApplication;
 
 public class Login extends AppCompatActivity {
+
+    public static int NUMBER_FRAGMENT_PRESENTATION = 3;
 
     @SuppressWarnings("WeakerAccess")
     private static CallbackManager callbackManager;
@@ -39,6 +41,11 @@ public class Login extends AppCompatActivity {
     private final ModelApplication modelApplication = ModelApplication.getModelApplication();
     private final ConnectionControler controlerConnection = ConnectionControler.getConnectionControler();
     private LoginButton loginButton;
+    private List<PresentationAppFragment> presentationApp;
+    private int indiceFragment;
+    private FragmentManager fragmentManager;
+
+
     /**
      * ATTENTION: This was auto-generated to implement the App Indexing API.
      * See https://g.co/AppIndexing/AndroidStudio for more information.
@@ -59,6 +66,7 @@ public class Login extends AppCompatActivity {
         callbackManager = CallbackManager.Factory.create();
         loginButton = (LoginButton) findViewById(R.id.facebook_login_button);
         addPermissions();
+        defineActionOnClick ();
 
         loginButton.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
             @SuppressWarnings("unused")
@@ -75,7 +83,8 @@ public class Login extends AppCompatActivity {
                                         .getCurrentAccessToken()
                                         .getToken(), profile2
                                         .getId(), GlobalSetting.USER_API, false);
-                                Toast.makeText(getApplicationContext(), getString(R.string.connexion_facebook_pending), Toast.LENGTH_SHORT).show();
+                                Toast.makeText(getApplicationContext(), getString(R.string
+                                        .connexion_facebook_pending), Toast.LENGTH_SHORT).show();
 
                             }
                         }
@@ -86,7 +95,8 @@ public class Login extends AppCompatActivity {
                     controlerConnection.sendPost(currentActivity.get(), AccessToken.getCurrentAccessToken().getToken(),
                             profile.getId(),
                             GlobalSetting.USER_API, false);
-                    Toast.makeText(getApplicationContext(), getString(R.string.connexion_facebook_pending), Toast.LENGTH_SHORT).show();
+                    Toast.makeText(getApplicationContext(), getString(R.string.connexion_facebook_pending), Toast
+                            .LENGTH_SHORT).show();
                 }
             }
 
@@ -98,14 +108,86 @@ public class Login extends AppCompatActivity {
 
             @Override
             public void onError(FacebookException error) {
-                Toast.makeText(getApplicationContext(), "Error occurred while logging in. Please try again.", Toast.LENGTH_SHORT).show();
+                Toast.makeText(getApplicationContext(), "Error occurred while logging in. Please try again.", Toast
+                        .LENGTH_SHORT).show();
                 Log.i("onError()", "Facebook login error.");
             }
         });
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
         client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
+
+    /**
+     * iniate the value in the fragment.
+     */
+    /**
+     * iniate the value in the fragment.
+     */
+    private void initiateFragment(){
+
+        // create fragment
+        PresentationAppFragment presentationfragmentUser = new PresentationAppFragment();
+        PresentationAppFragment presentationfragmentPeople = new PresentationAppFragment();
+        PresentationAppFragment presentationfragmentInteraction = new PresentationAppFragment();
+
+        // set parameters
+        presentationfragmentUser.setDrawableId(R.mipmap.music_image);
+        presentationfragmentUser.setPresentation(R.string.presentation_music);
+        presentationfragmentUser.setStateLocation(1);
+
+        presentationfragmentPeople.setDrawableId(R.mipmap.people_image);
+        presentationfragmentPeople.setPresentation(R.string.presentation_people);
+        presentationfragmentPeople.setStateLocation(2);
+
+        presentationfragmentInteraction.setDrawableId(R.mipmap.interaction_image);
+        presentationfragmentInteraction.setPresentation(R.string.presentation_interaction);
+        presentationfragmentInteraction.setStateLocation(3);
+
+        // add to list
+        presentationApp = new ArrayList<>();
+
+        presentationApp.add(presentationfragmentUser);
+        presentationApp.add(presentationfragmentPeople);
+        presentationApp.add(presentationfragmentInteraction);
+
+        indiceFragment = 0;
+        fragmentManager = getFragmentManager();
+    }
+
+    /**
+     * Increment the value of the indice.
+     */
+    private void incrementIndiceFragment(){
+        indiceFragment ++;
+        indiceFragment = indiceFragment% NUMBER_FRAGMENT_PRESENTATION;
+    }
+
+    /**
+     * Define the action on click on frame Layout
+     * The method should change the fragment inside to continue the presentation.
+     */
+    private void defineActionOnClick () {
+
+        initiateFragment();
+
+        FragmentTransaction ft = fragmentManager.beginTransaction();
+        Fragment currentFragment = presentationApp.get(indiceFragment);
+        ft.replace(R.id.containFragmentPresentation,currentFragment, ""+indiceFragment);
+        ft.commit();
+        incrementIndiceFragment();
+
+        FrameLayout fragmentPresentation =  (FrameLayout) findViewById(R.id.containFragmentPresentation);
+        fragmentPresentation.setOnClickListener((new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                FragmentTransaction ft = fragmentManager.beginTransaction();
+                Fragment currentFragment = presentationApp.get(indiceFragment);
+                ft.replace(R.id.containFragmentPresentation,currentFragment, ""+indiceFragment);
+                ft.commit();
+                incrementIndiceFragment();
+            }
+        }));
+    }
+
 
 
     @SuppressWarnings("StatementWithEmptyBody")
@@ -123,41 +205,5 @@ public class Login extends AppCompatActivity {
         permissions.add("user_birthday");
         permissions.add("user_about_me");
         loginButton.setReadPermissions(permissions);
-    }
-
-    /**
-     * ATTENTION: This was auto-generated to implement the App Indexing API.
-     * See https://g.co/AppIndexing/AndroidStudio for more information.
-     */
-    public Action getIndexApiAction() {
-        Thing object = new Thing.Builder()
-                .setName("Login Page") // TODO: Define a title for the content shown.
-                // TODO: Make sure this auto-generated URL is correct.
-                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
-                .build();
-        return new Action.Builder(Action.TYPE_VIEW)
-                .setObject(object)
-                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
-                .build();
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        client.connect();
-        AppIndex.AppIndexApi.start(client, getIndexApiAction());
-    }
-
-    @Override
-    public void onStop() {
-        super.onStop();
-
-        // ATTENTION: This was auto-generated to implement the App Indexing API.
-        // See https://g.co/AppIndexing/AndroidStudio for more information.
-        AppIndex.AppIndexApi.end(client, getIndexApiAction());
-        client.disconnect();
     }
 }
