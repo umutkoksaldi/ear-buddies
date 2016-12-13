249ae9a7847bd9db43e3b3be9ade8f020275648c
Arnaud <arnaud.dupeyrat@gmail.com>
2016-12-13 23:12:54 +0100

    fix webView does not close, add method to test content of RecycleView

--- app/src/androidTest/java/ch/epfl/sweng/project/testUi/TestHistoryMusic.java
+++ app/src/androidTest/java/ch/epfl/sweng/project/testUi/TestHistoryMusic.java
@@ -3,11 +3,14 @@ package ch.epfl.sweng.project.testUi;
 import android.app.Activity;
 import android.app.ActivityManager;
 import android.content.ComponentName;
+import android.content.Intent;
 import android.support.test.espresso.Espresso;
+import android.support.test.espresso.NoMatchingViewException;
 import android.support.test.espresso.UiController;
 import android.support.test.espresso.ViewAction;
 import android.support.test.espresso.contrib.RecyclerViewActions;
 import android.support.test.espresso.core.deps.guava.collect.Iterables;
+import android.support.test.espresso.matcher.ViewMatchers;
 import android.support.test.runner.AndroidJUnit4;
 import android.support.test.runner.lifecycle.ActivityLifecycleMonitorRegistry;
 import android.support.test.runner.lifecycle.Stage;
@@ -19,10 +22,13 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 import ch.epfl.sweng.project.MainActivity;
 import ch.epfl.sweng.project.Model.ModelApplication;
+import ch.epfl.sweng.project.Model.Music;
 import ch.epfl.sweng.project.R;
 import ch.epfl.sweng.project.media.MusicHistoryTest;
 import ch.epfl.sweng.project.ModuleRequest.MockUserMainActivityRule;
@@ -33,11 +39,13 @@ import static android.content.Context.ACTIVITY_SERVICE;
 import static android.support.test.espresso.Espresso.onView;
 import static android.support.test.espresso.action.ViewActions.click;
 import static android.support.test.espresso.assertion.ViewAssertions.matches;
+import static android.support.test.espresso.matcher.ViewMatchers.hasDescendant;
 import static android.support.test.espresso.matcher.ViewMatchers.isDescendantOfA;
 import static android.support.test.espresso.matcher.ViewMatchers.isDisplayed;
 import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static android.support.test.espresso.matcher.ViewMatchers.withText;
 import static ch.epfl.sweng.project.utils.GlobalTestSettings.PROFILE_TAB;
+import static com.facebook.FacebookSdk.getApplicationContext;
 import static junit.framework.Assert.fail;
 import static org.hamcrest.Matchers.allOf;
 
@@ -49,6 +57,7 @@ import static org.hamcrest.Matchers.allOf;
 public class TestHistoryMusic {
 
     private final ModelApplication modelApplication = ModelApplication.getModelApplication();
+    public static String ARTIST = "Rihanna";
 
     @Rule
     public MockUserMainActivityRule mActivityRule = new MockUserMainActivityRule(MainActivity.class);
@@ -58,6 +67,7 @@ public class TestHistoryMusic {
     public void testHistoryFragment() {
 
         try {
+
             // Play a song that we know it's on Lastfm
             MusicHistoryTest.playSongIntent(mActivityRule.getActivity().getApplicationContext(), GlobalTestSettings
                     .ARTIST_NAME_REQUEST, GlobalTestSettings.MUSIC_NAME_REQUEST);
@@ -66,25 +76,47 @@ public class TestHistoryMusic {
                     isDescendantOfA(withId(R.id.tabLayoutMain)));
             onView(matcher).perform(click());
 
-            // Open history fragment ansd click on the first music, which should display its lastfm page
-/*            onView(withId(R.id.music_history_recyclerview)).perform(
-                    RecyclerViewActions.actionOnItemAtPosition(0, MyViewAction.clickChildViewWithId(R.id
-                            .single_row_music_history)));*/
-
-        // Close the webview activity and go back to the main activity
             Thread.sleep(1000);
+
+
+            onView(withId(R.id.music_history_recyclerview))
+                    .check(matches(hasDescendant(withText(ARTIST))));
+
+            // check if the list contain Rihanna elements.
+            Music music = new Music();
+            music.setArtist(ARTIST);
+            List<Music> items = new ArrayList<>();
+            items.add(music);
+            RecyclerViewInteraction.<Music>onRecyclerView(withId(R.id.music_history_recyclerview))
+                    .withItems(items)
+                    .check(new RecyclerViewInteraction.ItemViewAssertion<Music>() {
+                        @Override
+                        public void check(Music item, View view, NoMatchingViewException e) {
+                            matches(hasDescendant(withText(item.getArtist())))
+                                    .check(view, e);
+                        }
+                    });
+
+
+
+            // Open history fragment ansd click on the first music, which should display its lastfm page
+           onView(withId(R.id.music_history_recyclerview)).perform(
+                    RecyclerViewActions.actionOnItemAtPosition(0, MyViewAction.clickChildViewWithId(R.id
+                            .single_row_music_history)));
+
+            Thread.sleep(1000);
+
+            // CustomTabsIntent need to be close, because Espresso does not see the activity.
+            // FLAG_ACTIVITY_CLEAR_TOP --> https://developer.android.com/reference/android/content/Intent.html
+            Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+            mActivityRule.getActivity().startActivity(intent);
+
         } catch (Exception e) {
             Log.e("testHistoryFragment", e.getMessage());
             fail("Exception during the execution");
         }
-
-
-        // Test if the Web fragment on lastFm is opened.
-/*        ActivityManager am = (ActivityManager) mActivityRule.getActivity().getSystemService(ACTIVITY_SERVICE);
-        List<ActivityManager.RunningTaskInfo> taskInfo = am.getRunningTasks(1);
-        ComponentName componentInfo = taskInfo.get(0).topActivity;
-        Log.d("CURRENT Activity ::" , taskInfo.get(0).topActivity.getClassName()+"   Package Name :  "+componentInfo
-                .getPackageName());*/
     }
+
 
 }
