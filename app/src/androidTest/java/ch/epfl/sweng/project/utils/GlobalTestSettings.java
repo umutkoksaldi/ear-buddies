030ce36b371821b471685721ff136b491838eff1
merinorus <merinorus.antoine@gmail.com>
2016-12-14 15:24:15 +0100

    Corrected test: tab name changed from "Users" to "People"

--- app/src/androidTest/java/ch/epfl/sweng/project/utils/GlobalTestSettings.java
+++ app/src/androidTest/java/ch/epfl/sweng/project/utils/GlobalTestSettings.java
@@ -13,12 +13,6 @@ public class GlobalTestSettings {
     // ************************************************************************************************************************************************
     // Main activity
 
-    public static String USERS_TAB = "Users";
-    public static String PROFILE_TAB = "Profile";
-    public static String MAP_TAB = "Map";
-
-    // ************************************************************************************************************************************************
-
     // ************************************************************************************************************************************************
     // Mock Users Data
     public static final String MOCK_ID_FACEBOOK = "121620614972695";
@@ -29,21 +23,24 @@ public class GlobalTestSettings {
     public static final String MOCK_USER_PROFILE_PICTURE = "https://scontent-amt2-1.xx.fbcdn.net/v/t1" +
             ".0-9/419921_334072353301892_257076548_n" +
             ".jpg?oh=1da08f8d32b10b20958a3df2f18096fa&oe=5886B16B";
+
+    // ************************************************************************************************************************************************
     public static final String MOCK_USER_COVER_PICTURE = "";
     public static final String MOCK_USER_EMAIL = "please_send_spam@gmail.com";
     public static final int MOCK_USER_AGE = 22;
     public static final String MOCK_USER_FIRST_NAME = "Sweng";
     public static final String MOCK_USER_LAST_NAME = "Rocks";
-
     // Sample song to test
     public static final String ARTIST_NAME_REQUEST = "rihana";
     public static final String MUSIC_NAME_REQUEST = "rude boy";
     // This is what the server should answer
     public static final String ARTIST_NAME_RESPONSE = "Rihanna";
     public static final String MUSIC_NAME_RESPONSE = "Rude Boy";
+    public static String USERS_TAB = "People";
+    public static String PROFILE_TAB = "Profile";
+    public static String MAP_TAB = "Map";
 
     // ************************************************************************************************************************************************
-
 
     public static void createMockUser() {
         User mUser = new User();
